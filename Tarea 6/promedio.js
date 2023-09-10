9868457928514e67175254e3ce72c701a5a5f200
Diego Lopez <die_elias2012@hotmail.com>
2023-09-10 13:20:46 -0600

    Implementacion de condicion de numeros negativos, donde si existe en el arreglo, se saltaran y no se tomaran en cuenta en el promedio del arreglo

--- Tarea 6/promedio.js
+++ Tarea 6/promedio.js
@@ -2,21 +2,21 @@ function calcularPromedio(arr) {
 
     const A = [4, 3, 5, 7, 7, 4, 1];
     let promedio = 0;
+    let contadorNegativo = 0;
 
     if (arr.length !== 0) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
                 promedio += arr[i];
             } else {
-                console.log("El arreglo tiene un número negativo, no puedo seguir calculando el promedio");
-                break;
+                contadorNegativo ++;
             }
         }
     } else {
         console.log("Arreglo vacío");
     }
 
-    promedio /= arr.length;
+    promedio /= (arr.length - contadorNegativo);
     console.log(promedio);
 }
 
