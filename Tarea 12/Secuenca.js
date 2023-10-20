096330255e42bf470ffb6eee09ed2d4d043d111e
Diego Lopez <die_elias2012@hotmail.com>
2023-10-19 23:20:26 -0600

    Tarea 12

---
+++ Tarea 12/Secuenca.js
@@ -0,0 +1,10 @@
+function secuenciaRecursiva(n) {
+    if (n == 1) {
+      console.log(n);
+    } else {
+      console.log(n);
+      secuenciaRecursiva(n - 1);
+    }
+  }
+  
+  secuenciaRecursiva(10); 
